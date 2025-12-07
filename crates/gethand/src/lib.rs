1ff66231108f264c4b9b397cd9d8d24f63bf96a1
Dijikey <gavrilovaroslav294@gmai.com>
2025-12-08 01:31:19 +0300

    New derive DebugIf created

--- crates/gethand/src/lib.rs
+++ crates/gethand/src/lib.rs
@@ -55,3 +55,63 @@ pub fn getters_derive(input: TokenStream) -> TokenStream {
 
     TokenStream::from(expanded)
 }
+
+#[proc_macro_derive(DebugIf, attributes(skip))]
+pub fn debug_if_debug(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+    let generics = input.generics;
+
+    let fields = match input.data {
+        Data::Struct(data) => match data.fields {
+            Fields::Named(fields) => fields.named,
+            _ => panic!("DebugIf only for structs with named fields"),
+        },
+        _ => panic!("DebugIf only for structs"),
+    };
+
+    let mut fmt_fields = Vec::new();
+
+    for field in fields {
+        let field_name = field.ident.as_ref().unwrap();
+
+        fmt_fields.push(quote! {
+            .field(stringify!(#field_name), &self.#field_name)
+        });
+    }
+
+    let (impl_generics, ty_generics_impl, original_where_clause) = generics.split_for_impl();
+
+    let mut new_where_clause = original_where_clause.cloned();
+
+    for param in &generics.params {
+        if let syn::GenericParam::Type(type_param) = param {
+            let ident = &type_param.ident;
+
+            let new_predicate: WherePredicate = syn::parse_quote! {
+                #ident: std::fmt::Debug
+            };
+
+            if let Some(ref mut clause) = new_where_clause {
+                clause.predicates.push(new_predicate);
+            } else {
+                new_where_clause = Some(syn::parse_quote! {
+                    where #ident: std::fmt::Debug
+                });
+            }
+        }
+    }
+
+
+    let expanded = quote! {
+        impl #impl_generics Debug for #name #ty_generics_impl #new_where_clause {
+            fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+                f.debug_struct("Engine")
+                #(#fmt_fields)*
+                .finish()
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
