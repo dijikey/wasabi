72699e7ca632c0d62920bcf028cd1d326ec26fe2
Dijikey <gavrilovaroslav294@gmai.com>
2025-12-08 00:46:38 +0300

    Create mods: default(contains default implementation) and prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
 mod core;
+pub mod default;
+
+pub use core::*;
+
+pub mod traits{ pub use wasabi_traits::*; }
+pub mod prelude{
+    pub use crate::core::*;
+    pub use crate::traits::Layer;
+    pub use crate::traits::scene::*;
+}
+
 #[cfg(test)]
 mod test;
 
