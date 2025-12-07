9a1c883480429fa26f4d853daae6236d2542d580
Dijikey <gavrilovaroslav294@gmai.com>
2025-12-08 02:00:41 +0300

    Everything can still change.

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,26 +1,33 @@
-use crate::core::scene::{SceneCatalog, SceneFn};
-use gethand::Getters;
+use wasabi_traits::scene::SceneFn;
+use gethand::{DebugIf, Getters};
 use std::fmt::{Debug, Formatter};
+use crate::default::SceneManager as DefSceneManager;
+use wasabi_traits::scene::SceneCatalog;
 
 #[derive(Getters, DebugIf)]
-pub struct Engine<SceneManager = DefSceneManager>
+pub struct Engine<ResourceSystem, SceneManager = DefSceneManager>
 where
     SceneManager: SceneCatalog,
 {
     scene_manager: SceneManager,
+    resource_system: ResourceSystem
 }
-impl<SceneManager> Engine<SceneManager>
+impl<SceneManager, ResourceSystem> Engine<ResourceSystem, SceneManager>
 where
     SceneManager: SceneCatalog,
 {
-    pub fn new(scene_manager: SceneManager) -> Engine<SceneManager> {
-        Engine { scene_manager }
+    pub fn new(scene_manager: SceneManager, resource_system: ResourceSystem) -> Self {
+        Engine {
+            scene_manager,
+            resource_system
+        }
     }
 
     pub fn next_scene(&mut self) {
         self.scene_manager.next()
     }
 
+    // Todo: This fn should be deleted
     pub fn cycle(&mut self) {
         let scene = self.scene_manager.curr();
         for i in 0..scene.len() {
@@ -31,17 +38,11 @@ where
 }
 
 
-
-impl Default for Engine {
-    fn default() -> Self {
-        Self {
-            scene_manager: DefSceneManager::default(),
-        }
-    }
-}
-
-pub trait Layer: Debug {
-    fn render(&mut self);
-
-    fn update(&mut self);
-}
+// impl Default for Engine {
+//     fn default() -> Self {
+//         Self {
+//             scene_manager: DefSceneManager::default(),
+//         }
+//     }
+// }
+// 
