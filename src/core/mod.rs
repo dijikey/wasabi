176e841787ad39250a1820599892c53933e37911
Dijikey <gavrilovaroslav294@gmai.com>
2026-01-10 03:55:56 +0300

    Impl new trait `WindowContext` and create main function `run`

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,41 +1,75 @@
-use crate::default::SceneManager as DefSceneManager;
+use std::sync::{Arc, Mutex, MutexGuard};
+
 use gethand::{DebugIf, Getters};
-use std::fmt::{Debug, Formatter};
-use wasabi_traits::event::EventHandler;
+use wasabi_traits::WindowContext;
+use wasabi_traits::input::InputHandler;
 use wasabi_traits::scene::SceneCatalog;
-use wasabi_traits::scene::SceneFn;
+
+// #[allow(dead_code)]
+// pub mod builder;
+// pub use builder::builder;
+
+macro_rules! info {
+    ($($arg:tt)+) => {
+        log::info!($($arg)+)
+    };
+}
 
 #[derive(Getters, DebugIf)]
-pub struct Engine<EventSystem, SceneManager = DefSceneManager>
+pub struct Engine<EventSystem, SceneManager, Window>
 where
     SceneManager: SceneCatalog,
-    EventSystem: EventHandler,
+    EventSystem: InputHandler,
+    Window: WindowContext,
 {
     scene_manager: SceneManager,
-    event_system: EventSystem,
+    #[skip]
+    event_system: Arc<Mutex<EventSystem>>,
+    window: Window,
 }
-impl<SceneManager, EventSystem> Engine<EventSystem, SceneManager>
+
+impl<SceneManager, EventSystem, Window> Engine<EventSystem, SceneManager, Window>
 where
     SceneManager: SceneCatalog,
-    EventSystem: EventHandler,
+    Window: WindowContext,
+    EventSystem: InputHandler<Key = Window::Key, Action = Window::Action> + 'static,
 {
-    pub fn new(scene_manager: SceneManager, event_system: EventSystem) -> Self {
-        Engine {
+    pub fn new(scene_manager: SceneManager, event_system: EventSystem, window: Window) -> Self {
+        let mut engine = Engine {
             scene_manager,
-            event_system,
-        }
+            event_system: Arc::new(Mutex::new(event_system)),
+            window,
+        };
+
+        let cloned = engine.event_system.clone();
+
+        engine
+            .window
+            .hook_key_callback(move |key, action| match cloned.lock() {
+                Ok(mut result) => result.key_callback(key, action),
+                Err(mut err) => err.get_mut().key_callback(key, action),
+            });
+
+        info!("Engine initialized");
+        engine
+    }
+
+    pub fn event_system(&mut self) -> MutexGuard<'_, EventSystem> {
+        self.event_system.as_ref().lock().unwrap()
     }
 
     pub fn next_scene(&mut self) {
         self.scene_manager.next()
     }
 
-    // Todo: This fn should be deleted
-    pub fn cycle(&mut self) {
-        let scene = self.scene_manager.curr();
-        for i in 0..scene.len() {
-            scene.get(i).update();
-            scene.get(i).render();
+    pub fn run<F: FnMut()>(&mut self, mut func: F) {
+        while !self.window.should_close() {
+            func();
+
+            self.window.poll_events();
+            self.window.swap_buffer();
         }
     }
+
+    fn key_handler(&self, key: Window::Key, action: Window::Action) {}
 }
